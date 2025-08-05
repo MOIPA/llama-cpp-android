06c6a5a51616aa06a179d86c213aeb6e626491ab
TR <522024320139@smail.nju.edu.cn>
2025-08-05 17:19:06 +0800

    美化图片上传

--- app/src/main/java/com/example/myllm/MainActivity.kt
+++ app/src/main/java/com/example/myllm/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.myllm
 
 import android.app.ActivityManager
 import android.app.DownloadManager
-import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -14,25 +12,35 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
@@ -40,53 +48,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.core.content.getSystemService
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.myllm.ui.theme.MyLLMTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.colorResource
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlin.getValue
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.isImeVisible
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.platform.LocalWindowInfo
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.ImeAction
-
 
 class MainActivity(
     activityManager: ActivityManager? = null,
@@ -252,13 +236,11 @@ fun MainCompose(
 
 @Composable
 fun ImageWithCloseButton(viewModel: MainViewModel) {
+    val borderColor = Color(0xFFCCCCCC)
     Box(
         modifier = Modifier
-            .size(150.dp)
-            .background(colorResource(R.color.teal_700), shape = RoundedCornerShape(15.dp))
-//            .clip(RoundedCornerShape(25.dp))
-            .padding(10.dp),
-        contentAlignment = Alignment.TopEnd,
+            .padding(bottom = 10.dp)
+            .wrapContentSize(align = Alignment.TopStart),
     ) {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
@@ -266,22 +248,28 @@ fun ImageWithCloseButton(viewModel: MainViewModel) {
                 .crossfade(true)
                 .build(),
             contentDescription = "preview",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop, // 裁切图片
+            modifier = Modifier
+                .size(96.dp)    // 图片大小
+                .clip(RoundedCornerShape(16.dp)) // 圆角图片
+                .border(width = 3.dp, color = borderColor, shape = RoundedCornerShape(16.dp)),
             error = painterResource(R.drawable.ic_broken_image),
             placeholder = painterResource(R.drawable.loading_img),
         )
         IconButton(
             onClick = { viewModel.clearImage() },
             modifier = Modifier
+                .offset(x = 80.dp, y = (-10).dp)
                 .background(colorResource(R.color.white), shape = CircleShape)
-                .padding(3.dp)
-                .size(15.dp)
+                .border(width = 2.dp, color = borderColor, shape = CircleShape)
+                .size(24.dp)
+                .zIndex(1f)
         ) {
             Icon(
                 imageVector = Icons.Default.Close,
                 contentDescription = "Close",
-                tint = colorResource(R.color.black)
+                tint = colorResource(R.color.black),
+                modifier = Modifier.size(14.dp)
             )
         }
     }
